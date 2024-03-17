d8381d4e83baa05bacadee5a4b89089d7296d32f
FrancioXavier <francioxaviersilva@gmail.com>
2024-03-16 21:09:39 -0300

    exercicio web

---
+++ middleware/validatePet.js
@@ -0,0 +1,10 @@
+module.exports =  (req, res, next) => {
+    const body = req.body;
+    if(!body.species) return res.status(401).json({errors: 'errado'});
+    if(!body.age) return res.status(401).json({errors: 'errado'});
+    if(!body.breed) return res.status(401).json({errors: 'errado'});
+    if(!body.color) return res.status(401).json({errors: 'errado'});
+    if(!body.ownerName) return res.status(401).json({errors: 'errado'});
+    if(!body.contact) return res.status(401).json({errors: 'errado'});
+    next()
+}
